1f3fb30a8a7dd5b6254666d7ed88a06a05bd2f47
Pooja Shetty <poojashettypooja757@gmail.com>
2025-11-26 22:23:49 +0530

    first commit

---
+++ frontend/src/components/RelatedProducts.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductsItems from './ProductsItems';
+
+const RelatedProducts = ({category,subCategory}) => {
+
+   const { products } = useContext(ShopContext);
+   const [related,setRelated] = useState([]);
+
+   useEffect(()=>{
+
+    if (products.length > 0) {
+      
+      let productsCopy = products.slice();
+
+      productsCopy = productsCopy.filter((item) => item.category === category);
+      productsCopy = productsCopy.filter((item) => item.subCategory === subCategory);
+
+      setRelated(productsCopy.slice(0,5));
+
+    }
+
+
+   },[products,category, subCategory])
+
+
+  return (
+    <div className='my-24'>
+       <div className='text-center text-3xl py-2'>
+          <Title text1={'RELATED'} text2={"PRODUCTS"}/>
+       </div>
+
+
+       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+          {related.map((items,index)=>(
+            <ProductsItems key={index} id={items._id} name={items.name} price={items.price} image={items.image}/>
+          ))}
+       </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
